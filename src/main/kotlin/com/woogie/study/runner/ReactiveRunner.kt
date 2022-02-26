3379e3169917f3bdae91ee6865cebd2c790bd663
Woogie Kim <woogie.kim@cyworldlabs.com>
2022-02-27 01:29:59 +0900

    리액티브 API Runner

---
+++ src/main/kotlin/com/woogie/study/runner/ReactiveRunner.kt
@@ -0,0 +1,21 @@
+package com.woogie.study.runner
+
+import com.woogie.study.someClient
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Mono
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val log = LoggerFactory.getLogger("com.woogie.study.ReactiveRunner")
+
+    measureTimeMillis {
+        val bodies: List<Mono<String>> = (1 until 100).map { id ->
+            someClient.get().uri("/api/$id").retrieve().bodyToMono(String::class.java)
+                .doOnNext { log.info(it) }
+        }
+
+        // Mono.`when`(bodies).block()
+        val result = Mono.zip(bodies) { it.map { any -> any as String } }.block()!!
+        result.forEach { println(it) }
+    }.let { log.info("$it ms") }
+}
